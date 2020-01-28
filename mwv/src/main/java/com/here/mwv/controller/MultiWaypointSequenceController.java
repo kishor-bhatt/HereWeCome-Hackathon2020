2eed158d1948ff80a5b7a975ccfe540aabefaedc
kishorbhatt <kishor.bhatt@here.com>
2020-01-28 17:09:41 +0530

    Add missing file

--- mwv/src/main/java/com/here/mwv/controller/MultiWaypointSequenceController.java
+++ mwv/src/main/java/com/here/mwv/controller/MultiWaypointSequenceController.java
@@ -1,10 +1,28 @@
 package com.here.mwv.controller;
 
 
+import com.here.mwv.model.MultiVechileSequencingRequest;
+import com.here.mwv.model.MultiWayPointSequenceResponse;
+import com.here.mwv.service.MultiWaypointSequenceService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@Slf4j
 @RestController
-@RequestMapping("/api/calculateRoute'}")
+@RequestMapping("/api/getRoute")
 public class MultiWaypointSequenceController {
+
+    @Autowired
+    private MultiWaypointSequenceService multiWaypointSequenceService;
+    @GetMapping(value = "/", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<MultiWayPointSequenceResponse> get(@RequestBody MultiVechileSequencingRequest multiVechileSequencingRequest){
+// log.info("Request Body ",multiVechileSequencingRequest);
+
+        return ResponseEntity.ok(multiWaypointSequenceService.getMultiWayPointSeqenceResponsne(multiVechileSequencingRequest));
+
+    }
 }
